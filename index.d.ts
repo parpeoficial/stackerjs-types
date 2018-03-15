63bde3b3aa01f72cd6f8f2227affec512dcc6821
Rafael Rabaço <rafael.rabaco@parpe.com>
2018-03-15 10:45:57 -0300

    feature(new): implement new util functions

--- index.d.ts
+++ index.d.ts
@@ -821,6 +821,48 @@ export namespace StackerJS
      * Class responsible for implementing utils functions.
      */
     export namespace Utils {
+        
+        export namespace Text {
+            export function isString(value): boolean;
+
+            export function isLowercase(value): boolean;
+
+            export function isUppercase(value): boolean;
+
+            export function isBase64(value): boolean;
+
+            export function isIPAddress(value): boolean;
+
+            export function isJSON(value): boolean;
+
+            export function isEmailAddress(value): boolean;
+
+            export function isURI(value): boolean;
+        }
+
+        export namespace Arrays {
+            export function isArray(value): boolean;
+        }
+
+        export namespace Numbers {
+            export function isNumber(value): boolean;
+
+            export function isLessThan(value, comparator): boolean;
+
+            export function isGreaterThan(value, comparator): boolean;
+
+            export function isInteger(value): boolean;
+        }
+
+        export namespace Other {
+            export function isEmpty(value): boolean;
+
+            export function hasLength(value, length): boolean;
+
+            export function hasLengthRange(value, lower, upper): boolean;
+
+            export function matches(value, options): boolean;
+        }
 
         export namespace BR
         {
